d085011c5e0b0852b28f97edc1095dffce371e31
Rostyslav Yavorskyi <yavorskyi.rostik@gmail.com>
2024-07-31 01:20:33 +0300

    Auditorium endpoints for admins only

--- src/main/java/org/example/discussionrest/controller/AuditoriumController.java
+++ src/main/java/org/example/discussionrest/controller/AuditoriumController.java
@@ -7,6 +7,7 @@ import org.example.discussionrest.dto.AuditoriumUpdateDto;
 import org.example.discussionrest.exception.AuditoriumNotFoundException;
 import org.example.discussionrest.service.AuditoriumService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,28 +19,33 @@ public class AuditoriumController {
 
     private final AuditoriumService auditoriumService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public AuditoriumReadDto insert(@RequestBody AuditoriumCreateDto auditoriumCreateDto) {
         return auditoriumService.insertIfNotExists(auditoriumCreateDto);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public List<AuditoriumReadDto> findAll() {
         return auditoriumService.findAll();
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/{id}")
     public AuditoriumReadDto findById(@PathVariable int id) throws AuditoriumNotFoundException {
         return auditoriumService.findOne(id);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
     public void update(@PathVariable int id, @RequestBody AuditoriumUpdateDto auditoriumUpdateDto) throws AuditoriumNotFoundException {
         auditoriumService.update(id, auditoriumUpdateDto);
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable int id) throws AuditoriumNotFoundException {
