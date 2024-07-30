78cc9ba19321975bcbb658edb671aeac5de6f6cf
Rostyslav Yavorskyi <yavorskyi.rostik@gmail.com>
2024-07-30 05:43:29 +0300

    Second level cache

--- src/main/java/org/example/discussionrest/entity/Auditorium.java
+++ src/main/java/org/example/discussionrest/entity/Auditorium.java
@@ -1,11 +1,19 @@
 package org.example.discussionrest.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 @Data
 @Entity
 @Table(name = "auditorium")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Auditorium {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
