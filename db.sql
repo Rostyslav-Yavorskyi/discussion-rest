8cecc6afd9915e6f2205330a16878d0faf8cd988
Rostyslav Yavorskyi <yavorskyi.rostik@gmail.com>
2024-07-30 17:16:29 +0300

    User entity

--- db.sql
+++ db.sql
@@ -1,8 +1,19 @@
 create table auditorium (
-    id int not null AUTO_INCREMENT,
+    id int not null auto_increment,
     number int NOT NULL,
     primary key (id),
     unique key unique_constraint_number (number)
+);
+
+create table user (
+    id int not null auto_increment,
+    email varchar(320) not null,
+    password varchar(30) not null,
+    first_name varchar(30),
+    last_name varchar(30),
+    role varchar(10),
+    primary key (id),
+    unique key (email)
 );
 
 insert into auditorium (number) values(4);
--- src/main/java/org/example/discussionrest/entity/User.java
+++ src/main/java/org/example/discussionrest/entity/User.java
@@ -1,4 +1,34 @@
 package org.example.discussionrest.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Role role;
+
+    public enum Role {
+        USER,
+        ADMIN
+    }
 }
